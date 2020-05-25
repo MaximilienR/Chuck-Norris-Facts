45c4983b539f2faba176eca7471de6f07e01b6a8
MaximilienR <62236292+MaximilienR@users.noreply.github.com>
2020-05-25 15:54:10 +0200

    Add files via upload

---
+++ TP chuck/chuck.js
@@ -0,0 +1,46 @@
+"use strict";
+
+console.log ("TEST")
+
+let pushline = [ 'Chuck Norris ne ment jamais, c\'est la vérité qui se trompe.'
+                'Tu ne resistes pas à Chuck,c\'est Chuck qui resiste à la resistance  
+                'Chuck Norris a réussi à trouver la page 404.'
+                'Chuck Norris peut faire du feu en frottant deux glaçons.'
+                'Tous les soirs, les cauchemars font le même Chuck Norris.'
+                'Chuck Norris est la raison pour laquelle Charlie se cache.'
+                'Jésus Christ est né en 1940 avant Chuck Norris.'
+                'Un jour, Chuck Norris a perdu son alliance. Depuis, c\'est le bordel dans les Terres du Milieu.'
+                'Une larme de Chuck Norris peut guérir du cancer, malheureusement Chuck Norris ne pleure pas.'
+                'Un aigle peut lire un journal à 1400 mètres de distance. Chuck Norris peut tourner la page.'
+                'Chuck Norris est mort depuis 10 ans, mais la Mort n\'a pas encore trouvé le courage d\'aller lui dire.']
+                
+                function punchlineNext )
+                {
+                 
+                this.textContent = "bachibouzouk";
+                }
+                
+                let p = document.querySelector(".");
+                p.addEventListener("click",changeText);
+
+
+/*function generatepushline() {getElementById("id")
+                    return pushline[Math.floor(Math.random() * pushline.length)+color]
+            }
+            '#'+(Math.random()*0xFFFFFF<<0).toString(16)
+            function changeText()
+            {
+            // this is the current element
+            this.textContent = "HIDDEN MESSAGE";
+            }
+            
+            let p = document.querySelector(".text");
+            function changerCouleur()
+            {
+            document.forms["tt"].elements["test"].style.backgroundColor = '#00ff00';
+            document.forms["tt"].elements["test1"].style.backgroundColor = '#00ff00';
+            }v
+console.log ("return");
+
+
+	
